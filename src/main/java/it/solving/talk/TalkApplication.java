eed9e21d3db8e5153871f52aeb6cd63c45c8e3b9
Alessandro Moretto <a.moretto@solvingteam.it>
2020-02-09 19:48:59 +0100

    log

--- src/main/java/it/solving/talk/TalkApplication.java
+++ src/main/java/it/solving/talk/TalkApplication.java
@@ -1,14 +1,17 @@
 package it.solving.talk;
 
+import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@CommonsLog
 @SpringBootApplication
 public class TalkApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TalkApplication.class, args);
 
+        log.info(">>>>>>>>>>>>  WELCOME TO THE TALK  <<<<<<<<<<<<<<");
         System.out.println("WELCOME TO THE TALK");
     }
 
