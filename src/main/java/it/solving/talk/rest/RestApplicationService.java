ef2a71c55eb5c47028ab91036262109ee28e22e4
Alessandro Moretto <a.moretto@solvingteam.it>
2020-02-09 21:23:50 +0100

    added hostname in rest response

--- src/main/java/it/solving/talk/rest/RestApplicationService.java
+++ src/main/java/it/solving/talk/rest/RestApplicationService.java
@@ -1,16 +1,20 @@
 package it.solving.talk.rest;
 
+import lombok.SneakyThrows;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+
 @RestController
 @RequestMapping("/talk")
 public class RestApplicationService {
 
+    @SneakyThrows
     @GetMapping
     public ResponseEntity sayHello() {
-        return ResponseEntity.ok("Hello !");
+        return ResponseEntity.ok("This Host is:" + InetAddress.getLocalHost().getHostName());
     }
 }
